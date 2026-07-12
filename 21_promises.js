e571c60f7d2f582aca6f7d85b18ed9e533f22a66
Keshav <keshavkhandelwal30@gmail.com>
2026-07-12 15:39:10 +0530

    Promises

---
+++ 21_promises.js
@@ -0,0 +1,49 @@
+const promiseFour = new Promise(function (resolve, reject) {
+    // Do an async task: DB calls, cryptography, network
+    setTimeout(function () {
+        let ok = true;
+        if (ok) {
+            resolve({ username: "hitesh", password: "123" });
+        } else {
+            reject("ERROR: Something went wrong");
+        }
+    }, 1000);
+});
+
+promiseFour
+    .then((user) => {
+        console.log(user);
+        return user.username;
+    })
+    .then((username) => {
+        console.log(username);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .finally(() => console.log("The promise is either resolved or rejected"));
+
+// promise and async - await
+const promiseFive = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        let error = true;
+        if (!error) {
+            resolve({ username: "javascript", password: "123" });
+        } else {
+            reject("ERROR: JS went wrong");
+        }
+    }, 1000);
+});
+async function consumePromiseFive() {
+    try {
+        const response = await promiseFive;
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+consumePromiseFive();
+
+// promise.all: run promises in PARALLEL, wait for all
+// it takes an array of promises and executes them (i.e, either resolve or reject them, all in parallel)
+const [a, b] = await Promise.all([fetch(urlA), fetch(urlB)]);
